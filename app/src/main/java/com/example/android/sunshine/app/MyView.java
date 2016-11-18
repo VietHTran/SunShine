ecc6412458436e26a77885f34b8fdf421b4153aa
VietHTran <thanos.trn11@gmail.com>
2016-11-18 02:16:27 -0600

    Customize Accessibility for MyView

--- app/src/main/java/com/example/android/sunshine/app/MyView.java
+++ app/src/main/java/com/example/android/sunshine/app/MyView.java
@@ -2,21 +2,38 @@ package com.example.android.sunshine.app;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
 
 /**
  * Created by Viet on 11/17/2016.
  */
 public class MyView extends View {
+    Canvas canvas;
+
+    void sendAccessibility(Context context) {
+        AccessibilityManager accessibilityManager =
+                (AccessibilityManager) context.getSystemService(
+                        Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager.isEnabled()) {
+            sendAccessibilityEvent(
+                    AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+        }
+    }
     public MyView(Context context) {
         super(context);
+        sendAccessibility(context);
     }
     public MyView(Context context, AttributeSet attributeSet) {
         super(context,attributeSet);
+        sendAccessibility(context);
     }
     public MyView(Context context, AttributeSet attributeSet, int defaultStyle) {
         super(context,attributeSet,defaultStyle);
+        sendAccessibility(context);
     }
     @Override
     protected void onMeasure(int width, int height) {
@@ -30,6 +47,16 @@ public class MyView extends View {
     }
     @Override
     protected void onDraw(Canvas canvas) {
-
+        super.onDraw(canvas);
+        if (this.canvas==null) {
+            this.canvas= new Canvas();
+            this.canvas.drawCircle(50.0f,50.0f,10.0f,new Paint(Paint.ANTI_ALIAS_FLAG));
+        }
+        canvas=this.canvas;
+    }
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+        //Action
+        return true;
     }
 }
